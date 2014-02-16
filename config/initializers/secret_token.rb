aafb282fb253e80b59a19bee76b04a2c2e1c5c7e
Derrick Mar <derrickmar1215@berkeley.edu>
2014-02-16 01:15:22 -0800

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IntelHack::Application.config.secret_key_base = '0c554f6cfb931112f3732ae60c0b754104010b2b6bc80346e06c2a1ce99477e5f068e6cc265396a23ba00c7ddca00c2851d3509b3235c3ba34700df5a1ebd51a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+IntelHack::Application.config.session_store :cookie_store, key: '_intelHack_session'
